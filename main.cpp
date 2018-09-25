435017436d94a02a2f68eccbc98e9f7c852b5306
XuChen <xuchen@u.nus.edu>
2018-09-25 23:50:46 +0800

    Multi threads running properly on Shadow

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #include <assert.h>
 #include <sys/types.h>
@@ -19,11 +20,20 @@
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 
+/*  For multithreads */
+//#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include <pthread.h>
+
+
 #define MY_PORT 12345
 
 #include "main.h"
 
 
+
+using namespace std;
+using namespace xcoin;
 
 void on_write(evutil_socket_t socket, short flags, void * args )
 {
@@ -60,24 +70,32 @@ start_client(XState* state, char* server_host_name)
 {
 
     state->client.server_host_name_ = strndup(server_host_name, (size_t) 50);
+    printf("server_host_name: %s\n", state->client.server_host_name_);
     /*  get address */ 
     struct addrinfo* server_info;
     int res = getaddrinfo(state->client.server_host_name_, NULL, NULL, &server_info);
     if(res == -1) {
         //error
-        return -1;
+        printf("err in address\n");
+        exit(1);
     }
     state->client.server_ip_ = ((struct sockaddr_in*) (server_info->ai_addr))->sin_addr.s_addr;
+
+    char str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(state->client.server_ip_), str, INET_ADDRSTRLEN);
+    printf("server ip: %s\n", str);
+
     freeaddrinfo(server_info);
 
-    state->client.sd_ = socket(AF_INET, SOCK_STREAM, 0);
+    state->client.sd_ = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (state->client.sd_ == -1) {
         //error
+        printf("start_client: unable to open socket\n");
         return -1;
     }
 
     /*  client socket address info */
-    struct sockaddr_in listen_addr;
+    sockaddr_in listen_addr;
     memset(&listen_addr, 0, sizeof(listen_addr));
 
     listen_addr.sin_family = AF_INET;
@@ -85,8 +103,9 @@ start_client(XState* state, char* server_host_name)
     listen_addr.sin_port=htons(MY_PORT);
 
     /*  Connect to the server */
-    res = connect(state->client.sd_, (struct socketaddr *) &listen_addr, sizeof(listen_addr));
+    res = connect(state->client.sd_, (sockaddr *) &listen_addr, sizeof(listen_addr));
     if (res == -1 && errno != EINPROGRESS) {
+        printf("start_client: failed to connect socket\n");
         return -1;
     }
 
@@ -97,7 +116,7 @@ start_client(XState* state, char* server_host_name)
     ev_read = event_new(base, state->client.sd_, EV_READ | EV_PERSIST, on_read, &state->client);
     ev_write= event_new(base, state->client.sd_, EV_WRITE, on_write, NULL); /* Write once */
 
-    event_add(ev_read, NULL);
+    //event_add(ev_read, NULL); /*  No echoing back yet */
     event_add(ev_write, &tv); 
     event_base_dispatch(base);
 
@@ -108,26 +127,28 @@ void on_accept(int socket, short ev, void *arg)
 {
     int client_fd;
     struct sockaddr_in client_addr;
+    struct event_base * base = (struct event_base*) arg;
     Client* client;
     socklen_t client_len = sizeof(client_addr);
-
+    printf("Accepting conn \n");
 
     client_fd = accept(socket, (struct sockaddr *)&client_addr, &client_len);
+    printf("Accepted \n");
     if (client_fd < 0) {
         printf("err: client_fd < 0\n");
         return;
     }
 
-    client = calloc(1, sizeof(*client));
+    client = (Client *) calloc(1, sizeof(Client));
     if (client == NULL) {
         printf("err: client calloc failed\n");
         exit(1);
     }
 
-    event_set(&client->ev_read_, client_fd, EV_READ | EV_PERSIST, on_read, client);
-    event_add(&client->ev_read_, NULL);
-    
-    printf("Accepted conn from %s\n", inet_ntoa(client_addr.sin_addr));
+    //printf("Accepted conn from %s\n", inet_ntoa(client_addr.sin_addr));
+    client->base_ = base; 
+    struct event * ev_read = event_new(base, client_fd, EV_READ | EV_PERSIST, on_read, client);
+    event_add(ev_read, NULL);
 }
 
 int 
@@ -147,7 +168,7 @@ start_server(XState * state)
     bind_address.sin_port = htons(MY_PORT);
 
     /*  bind the socket to ther server port */
-    int res = bind(state->server.sd_, (struct socketaddr *) &bind_address, sizeof(bind_address));
+    int res = bind(state->server.sd_, (struct sockaddr *) &bind_address, sizeof(bind_address));
     if (res == -1) {
         //failed
         return -1;
@@ -161,9 +182,10 @@ start_server(XState * state)
     struct event *ev_accept; 
     struct event_base *base = event_base_new();
 
-    ev_accept = event_new(base, state->server.sd_, EV_READ | EV_PERSIST, on_accept, (char*) "ACCEPTING CONN");
+    ev_accept = event_new(base, state->server.sd_, EV_READ | EV_PERSIST, on_accept, base);
     event_add(ev_accept, NULL);
-
+    
+    printf("Waiting for connection \n");
     event_base_dispatch(base);
 
     return 0;
@@ -171,7 +193,17 @@ start_server(XState * state)
 
 
 
+void *
+mine(void* interval) 
+{
+    long n= (long)interval;
+    while(true) {
+        sleep(n);
+        cout << "wake up liao" << endl;
+    }
 
+    pthread_exit(NULL);
+}
 
 
 XState*
@@ -180,7 +212,7 @@ init(int argc, char* argv[])
     /*  FIXME: check input */
     int res; 
 
-    XState *state = calloc(1, sizeof(XState));
+    XState *state = static_cast<XState*>(calloc(1, sizeof(XState)));
     assert(state);
     
 
@@ -200,7 +232,17 @@ init(int argc, char* argv[])
 int main(int argc, char*argv[])
 {
     printf("Starting....\n");
-    XState *state = init(argc, argv);
+    //boost::thread t1 { init, argc, argv };
+    pthread_t t1;
+    pthread_t t2;
+    long time = 100;
+    void *status;
 
-    return 0;
+    pthread_create(&t1, NULL, mine, (void *) time);
+    pthread_create(&t2, NULL, mine, (void *) time);
+
+    pthread_join(t1, &status);
+    pthread_join(t2, &status);
+
+    pthread_exit(NULL);
 }
--- main.h
+++ main.h
@@ -1,25 +1,29 @@
 #ifndef XCOIN_MAIN_H_
 #define XCOIN_MAIN_H_
-typedef struct _XState XState;
-typedef struct _Client Client;
-typedef struct _Server Server;
+
+namespace xcoin {
+    typedef struct _XState XState;
+    typedef struct _Client Client;
+    typedef struct _Server Server;
 
 
-struct _Server {
-    int sd_;
-};
+    struct _Server {
+        int sd_;
+    };
 
-struct _Client {
-    int sd_;
-    char* server_host_name_;
-    in_addr_t server_ip_;
-    struct event_base* base_;
-    struct event* ev_read_;
-};
+    struct _Client {
+        int sd_;
+        char* server_host_name_;
+        in_addr_t server_ip_;
+        struct event_base* base_;
+        struct event* ev_read_;
+    };
 
-struct _XState {
-    Server server;
-    Client client;
-};
+    struct _XState {
+        Server server;
+        Client client;
+    };
+
+}
 
 #endif
